25ef4f24835983a56e10ef5570d6e719cbd9e19b
Vinicius Benedito <beneditoloura@gmail.com>
2023-05-10 07:50:39 -0300

    feat: create and using AxiosAdapter

---
+++ frontend/src/infra/http/AxiosAdapter.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import HttpClient from "./HttpClient";
+
+export default class AxiosAdapter implements HttpClient {
+  async get(url: string): Promise<any> {
+    const response = await axios.get(url)
+    return response.data
+  }
+
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import TodosGatewayHttp from './infra/gateway/TodosGatewayHttp'
+import AxiosAdapter from './infra/http/AxiosAdapter'
 
 const app = createApp(App)
-const todosGateway = new TodosGatewayHttp()
+const httpClient = new AxiosAdapter()
+const baseUrl = 'http://localhost:3000'
+const todosGateway = new TodosGatewayHttp(httpClient, baseUrl)
 app.provide('todosGateway', todosGateway)
 app.mount('#app')
