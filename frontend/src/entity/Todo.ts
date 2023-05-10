e1ea19fb8628104a4ef7fdb50361650c642436ab
Vinicius Benedito <beneditoloura@gmail.com>
2023-05-10 07:37:46 -0300

    feat: create description validate

--- frontend/src/entity/Todo.ts
+++ frontend/src/entity/Todo.ts
@@ -1,5 +1,7 @@
 export default class Todo {
-  constructor(readonly description: string, public done: boolean = false) {}
+  constructor(readonly description: string, public done: boolean = false) {
+    if (description === "") throw new Error('Invalid description')
+  }
 
   toggleDone() {
     this.done = !this.done
