6f505f1c823c7bc6f32c5c732114779763d92682
Vinicius Benedito <beneditoloura@gmail.com>
2023-05-10 07:50:59 -0300

    refactor: removing axios and using HttpClient interface

--- frontend/src/infra/gateway/TodosGatewayHttp.ts
+++ frontend/src/infra/gateway/TodosGatewayHttp.ts
@@ -1,11 +1,12 @@
-import axios from "axios";
 import TodosGateway from "./TodosGateway";
+import HttpClient from "../http/HttpClient";
 
 export default class TodosGatewayHttp implements TodosGateway {
+  constructor(readonly httpClient: HttpClient, readonly baseUrl: string) {}
+
   
   async getTodos(): Promise<any> {
-    const response = await axios.get('http://localhost:3000/todos')
-    const todosData = response.data
+    const todosData = await this.httpClient.get(`${this.baseUrl}/todos`)
     return todosData
   }
 }
