7cb2b936a655fa1e2f2872824d70499e396c4dce
Vinicius Benedito <beneditoloura@gmail.com>
2023-05-15 21:31:03 -0300

    feature: pass setTodo to Todo class and update state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,31 +4,39 @@ import { useEffect, useState } from "react";
 
 export const App = () => {
   const { todosGateway } = useHttpClient();
-  const [total, setTotal] = useState(0)
-  const [completed, setCompleted] = useState(0)
-  const [description, setDescription] = useState('')
+  const [total, setTotal] = useState(0);
+  const [todoList, setTodoList] = useState(new TodoList(setTotal));
+  const [completed, setCompleted] = useState(0);
+  const [description, setDescription] = useState("");
 
-  let todoList: TodoList;
   useEffect(() => {
     const fetchData = async () => {
-      todoList = new TodoList()
       const todosData = await todosGateway.getTodos();
       todoList.addTodos(todosData);
-      setTotal(todoList.getTotal())
-      setCompleted(todoList.getCompleted())
-    }
+      setTodoList(todoList);
+      setTotal(todoList.getTotal());
+      setCompleted(todoList.getCompleted());
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h2>Total: {total}</h2>
-	    <h2>Completed: {completed}%</h2>
+      <h2>Completed: {completed}%</h2>
 
-
-      <input role="input" type="text" onChange={e => setDescription(e.target.value)}  />
-      <button className="add-todo-button" onClick={() => todoList.addTodo(description)}>Add</button>
+      <input
+        role="input"
+        type="text"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button
+        className="add-todo-button"
+        onClick={() => todoList.addTodo(description)}
+      >
+        Add
+      </button>
     </div>
   );
 };
