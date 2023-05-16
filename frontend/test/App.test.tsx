967a0d261498afbbad135edc1e1b53e17906a545
Vinicius Benedito <beneditoloura@gmail.com>
2023-05-15 21:36:07 -0300

    test: should render initial todo list

--- frontend/test/App.test.tsx
+++ frontend/test/App.test.tsx
@@ -1,44 +1,21 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import TodosGateway from '../src/infra/gateway/TodosGateway.js'
-import { App } from '../src/App'
-import React from 'react';
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { App } from "../src/App";
+import React from "react";
 
 function sleep(time: number) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(true)
-    }, time)
-  })
+      resolve(true);
+    }, time);
+  });
 }
 
-test('Should render initial todo list', async () => {
-  const { findByRole } = render(<App />)
+test("Should render initial todo list", async () => {
+  const { findByRole } = render(<App />);
 
-  // await sleep(100)
-  expect(await findByRole('heading', { name: 'Total: 1' })).toBeInTheDocument()
-  expect(await findByRole('heading', { name: 'Completed: 0%' })).toBeInTheDocument()
-})
-
-test('Should not let insert duplicated todo list', async () => {
-  const { findByRole, getByRole, container } = render(<App />)
-  const todosGateway: TodosGateway = {
-    async getTodos (): Promise<any> {
-      return [
-
-        { description: 'My first todo', done: false }
-      ]
-    }
-  }
-
-  await sleep(500)
-  const input = screen.getByRole('input')
-  const button = container.getElementsByClassName('add-todo-button')[0]
-  fireEvent.change(input, { target: { value: 'A' }})
-  fireEvent.click(button)
-  fireEvent.change(input, { target: { value: 'A' }})
-  fireEvent.click(button)
-
-  expect(await findByRole('heading', { name: 'Total: 1' })).toBeInTheDocument()
-  expect(await findByRole('heading', { name: 'Completed: 0%' })).toBeInTheDocument()
-})
+  expect(await findByRole("heading", { name: "Total: 1" })).toBeInTheDocument();
+  expect(
+    await findByRole("heading", { name: "Completed: 0%" })
+  ).toBeInTheDocument();
+});
